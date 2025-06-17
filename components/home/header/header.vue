5cd9d940d826dd5f6cd46d10ca43dc4d03f47f25
Pavle Jovanovic <44483259+minzhoudu@users.noreply.github.com>
2025-06-17 17:09:53 +0200

    [HOTFIX]: refactor home header into separate component

---
+++ components/home/header/header.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { getLocalTimeZone, type DateValue } from "@internationalized/date";
+
+const startValue = ref<DateValue>();
+
+const startLocations = ref([{ from: "Krusevac", to: "Beograd", search: "Krusevac Beograd" }, { from: "Krusevac", to: "Novi Sad", search: "Krusevac Novi Sad" }, { from: "Krusevac", to: "Subotica", search: "Krusevac Subotica" }]);
+const startLocation = ref({ from: "Krusevac", to: "Beograd", search: "Krusevac Beograd" });
+
+const { currentDate } = useCurrentDate();
+
+const isStartDateUnavailable = (date: DateValue) => {
+	return date.toDate(getLocalTimeZone()) < currentDate.value.toDate(getLocalTimeZone());
+};
+</script>
+
+<template>
+	<div class="flex flex-col md:flex-row justify-center items-center gap-5 bg-warning-300 p-4 lg:rounded-xl">
+		<USelectMenu
+			v-model="startLocation"
+			:items="startLocations"
+			class="min-w-48 py-2"
+			color="neutral"
+			:filter-fields="['search']"
+		>
+			<template #default="{ modelValue }">
+				{{ modelValue?.from }}
+				<Icon
+					name="i-lucide:arrow-right"
+					size="14"
+				/>
+				{{ modelValue?.to }}
+			</template>
+
+			<template #item="{ item }">
+				<div class="flex items-center gap-2">
+					{{ item.from }}
+					<Icon
+						name="i-lucide:arrow-right"
+						size="14"
+					/>
+					{{ item.to }}
+				</div>
+			</template>
+		</USelectMenu>
+
+		<AppDatePicker
+			v-model="startValue"
+			:is-date-unavailable="isStartDateUnavailable"
+			placeholder="Datum polaska"
+		/>
+	</div>
+</template>

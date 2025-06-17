a875c47141a4cf1f8fae19b335aa544e5b6512b9
Pavle Jovanovic <44483259+minzhoudu@users.noreply.github.com>
2025-06-17 12:10:05 +0200

    [HOTFIX] remove lock file

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,34 @@
 <script setup lang="ts">
-const value = ref("");
+const calendarValue = ref();
 </script>
 
 <template>
-	<div class="container">
-		<h1>Home Page</h1>
-		<UInput
-			v-model="value"
-			placeholder="Search..."
-			variant="subtle"
-			size="xl"
-			trailing-icon="i-lucide-search"
-			loading-icon="i-lucide-loader"
-		/>
+	<div class="container flex flex-col justify-center gap-10">
+		<div class="w-fit mx-auto my-4 py-3 px-4 rounded-xl bg-warning-300">
+			Home page
+		</div>
+
+		<div class="w-60 mx-auto">
+			<UCalendar
+				v-model="calendarValue"
+				initial-focus
+				color="warning"
+				range
+				:ui="{
+					headCell: 'text-warning-300',
+					cellTrigger: 'data-today:not-data-[selected]:text-warning-300 data-[selected]:bg-warning-300',
+				}"
+			/>
+
+			<h1 v-if="calendarValue">
+				Choosen date
+			</h1>
+
+			<div v-if="calendarValue">
+				Start: {{ calendarValue['start'] }}
+				<br>
+				End: {{ calendarValue['end'] }}
+			</div>
+		</div>
 	</div>
 </template>

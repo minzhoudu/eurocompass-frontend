99f04ce0eb00433419d340cd1a1a08d3984bd378
Pavle Jovanovic <44483259+minzhoudu@users.noreply.github.com>
2025-06-17 16:00:23 +0200

    date picker + select route

---
+++ components/app/date-picker/date-picker.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import type { DateValue } from "@internationalized/date";
+import { DateFormatter, getLocalTimeZone } from "@internationalized/date";
+
+type DatePickerProps = {
+	modelValue?: DateValue;
+	isDateUnavailable?: (date: DateValue) => boolean;
+	placeholder?: string;
+};
+
+const props = withDefaults(defineProps<DatePickerProps>(), {
+	placeholder: "Izaberite datum",
+});
+
+const emit = defineEmits<{
+	(e: "update:modelValue", value: DateValue | undefined): void;
+}>();
+
+const df = new DateFormatter("sr-RS", {
+	dateStyle: "full",
+});
+
+const dateValue = computed({
+	get: () => props.modelValue,
+	set: value => emit("update:modelValue", value),
+});
+</script>
+
+<template>
+	<UPopover>
+		<UButton
+			color="neutral"
+			class="bg-white font-bold cursor-pointer md:py-2 md:px-4"
+			variant="subtle"
+			icon="i-lucide-calendar"
+		>
+			{{ dateValue ? df.format(dateValue.toDate(getLocalTimeZone())) : placeholder }}
+		</UButton>
+
+		<template #content>
+			<UCalendar
+				v-model="dateValue"
+				initial-focus
+				color="warning"
+				:ui="{
+					headCell: 'text-warning-300',
+					cellTrigger: 'data-today:not-data-[selected]:text-warning-300 data-[selected]:bg-warning-300',
+				}"
+				:is-date-unavailable="isDateUnavailable"
+			/>
+		</template>
+	</UPopover>
+</template>

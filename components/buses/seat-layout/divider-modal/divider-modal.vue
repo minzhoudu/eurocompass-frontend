3f0ea32577cdf9a5bd1a61defeaabc038dbcedd5
Pavle Jovanovic <44483259+minzhoudu@users.noreply.github.com>
2025-07-05 16:19:02 +0200

    [FIX] autofocus input in devider modal, save on enter key

--- components/buses/seat-layout/divider-modal/divider-modal.vue
+++ components/buses/seat-layout/divider-modal/divider-modal.vue
@@ -6,14 +6,24 @@ const open = ref(false);
 const emit = defineEmits<{
 	(e: "update:divider", text: string): void;
 }>();
+
+const handleUpdateDivider = (event: KeyboardEvent) => {
+	if (!dividerText.value) return;
+
+	if (event.key === "Enter") {
+		emit("update:divider", dividerText.value);
+		open.value = false;
+		dividerText.value = "";
+	}
+};
 </script>
 
 <template>
 	<UModal
 		title="Naziv sprata"
-		description="Unesite naziv sprata. (npr. prvi sprat, donji sprat...)"
+		description="Unesite naziv sprata. (npr. I, II, prvi sprat, donji sprat...)"
 		:open="open"
-		:close="{ onClick: () => { open = false } }"
+		@update:open="(value) => { open = value }"
 	>
 		<Icon
 			name="tabler:separator-horizontal"
@@ -23,10 +33,15 @@ const emit = defineEmits<{
 		/>
 
 		<template #body>
-			<div class="flex gap-2 p-5">
+			<div
+				class="flex gap-2 p-5"
+				@keydown="handleUpdateDivider"
+			>
 				<UInput
 					v-model="dividerText"
+					:autofocus="true"
 					class="w-full"
+					leading-icon="tabler:separator-horizontal"
 				/>
 
 				<UButton

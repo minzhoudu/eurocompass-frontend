09b7ad9e4de3a6471b875a617d00bed27e5d9745
Vojin <puricvojin@gmail.com>
2025-08-04 22:04:20 +0200

    Assign orphan call

--- components/admin-dashboard/admin-dashboard.vue
+++ components/admin-dashboard/admin-dashboard.vue
@@ -14,17 +14,14 @@ const expandedRoutes = ref<string[]>([]);
 	<template v-if="busInfoError || getRidesError">
 		<div class="flex flex-col items-center w-full bg-warning-300 text-error font-semibold py-3">
 			<h1 class="text-lg">
-				Došlo je do greške pri dobavljanju informacija o {{ busInfoError ? "autobusima" : "polascima" }}. Pokušajte ponovo.
+				Došlo je do greške pri dobavljanju informacija o {{ busInfoError ? "autobusima" :
+					"polascima" }}. Pokušajte ponovo.
 			</h1>
 			<p class="text-error/90">
 				Ako se greška ponovi, molimo prijavite problem.
 			</p>
 
-			<UButton
-				label="Pokušajte ponovo"
-				class="cursor-pointer mt-4"
-				@click="() => $router.go(0)"
-			/>
+			<UButton label="Pokušajte ponovo" class="cursor-pointer mt-4" @click="() => $router.go(0)" />
 		</div>
 	</template>
 
@@ -36,95 +33,74 @@ const expandedRoutes = ref<string[]>([]);
 				</h1>
 			</div>
 
-			<div
-				v-if="busesLoading || getRidesLoading"
-				class="flex flex-col gap-10 lg:flex-row w-full h-96 lg:h-40 lg:px-10"
-			>
+			<div v-if="busesLoading || getRidesLoading"
+				class="flex flex-col gap-10 lg:flex-row w-full h-96 lg:h-40 lg:px-10">
 				<USkeleton class="flex-1 h-40" />
 				<USkeleton class="flex-1 h-40" />
 			</div>
 
-			<div
-				v-else
-				class="grid grid-cols-1 lg:grid-cols-2 w-full lg:justify-around gap-x-5 lg:gap-x-0 gap-y-10 lg:gap-y-30 lg:justify-items-center"
-			>
-				<div
-					v-for="route in routesWithRides"
-					:key="route.id"
-				>
-					<UTree
-						v-model:expanded="expandedRoutes"
-						:items="getTreeItems(route)"
+			<div v-else
+				class="grid grid-cols-1 lg:grid-cols-2 w-full lg:justify-around gap-x-5 lg:gap-x-0 gap-y-10 lg:gap-y-30 lg:justify-items-center">
+				<div v-for="route in routesWithRides" :key="route.id">
+					<UTree v-model:expanded="expandedRoutes" :items="getTreeItems(route)"
 						class="border rounded-lg w-full lg:w-[500px] max-h-96 scroll-bar"
-						:ui="{ listWithChildren: 'border-warning-300' }"
-					>
-						<template #bus-item="{ item }: { item: {bus: ExtendedBus, rideId: string} }">
-							<UTooltip
-								:content="{ side: 'top' }"
-								:delay-duration="0"
-								:ui="{
-									content: 'bg-primary',
-								}"
-							>
+						:ui="{ listWithChildren: 'border-warning-300' }">
+						<template
+							#bus-item="{ item }: { item: { bus: ExtendedBus, rideId: string } }">
+							<UTooltip :content="{ side: 'top' }" :delay-duration="0" :ui="{
+								content: 'bg-primary',
+							}">
 								<div class="flex flex-col gap-2 w-full">
-									<div class="bg-primary text-white/90 p-2 rounded flex justify-between items-center lg:gap-4 font-bold flex-wrap md:flex-nowrap">
-										<Icon
-											name="lucide:bus"
-											size="20"
-										/>
+									<div
+										class="bg-primary text-white/90 p-2 rounded flex justify-between items-center lg:gap-4 font-bold flex-wrap md:flex-nowrap">
+										<Icon name="lucide:bus" size="20" />
 
 										<p>
 											{{ item.bus.name }}
 										</p>
-										<p>Slobodnih mesta - {{ item.bus.freeSeats }}</p>
+										<p>Slobodnih mesta - {{
+											item.bus.freeSeats }}</p>
 
-										<div
-											v-if="buses && item.bus && item.bus.busId"
-											class="flex gap-1"
-										>
+										<div v-if="buses && item.bus && item.bus.busId"
+											class="flex gap-1">
 											<AdminDashboardChangeBusSelector
 												:buses="buses"
 												:current-bus="item.bus"
 												:ride-id="item.rideId"
-												@bus-changed="refetchGetRides"
-											/>
+												@bus-changed="refetchGetRides" />
 
 											<AdminDashboardRemoveBusModal
 												:bus-id="item.bus.busId"
 												:ride-id="item.rideId"
-												@bus-removed="refetchGetRides"
-											/>
+												@bus-removed="refetchGetRides" />
 										</div>
 									</div>
 								</div>
 
 								<template #content>
-									<div class="flex flex-col gap-2 text-white/90 px-2 py-1 rounded">
-										<p>Registracija: <span class="font-bold">{{ item.bus.registration }}</span></p>
+									<div
+										class="flex flex-col gap-2 text-white/90 px-2 py-1 rounded">
+										<p>Registracija: <span
+												class="font-bold">{{
+													item.bus.registration
+												}}</span></p>
 									</div>
 								</template>
 							</UTooltip>
 						</template>
 
-						<template #ride-item="{ item }: { item: { label: string, icon: string, orphans: Orphan[], rideId: string, buses: Bus[] } }">
-							<AdminDashboardRideItem
-								:icon="item.icon"
-								:label="item.label"
-								:orphans="item.orphans"
-								:buses="item.buses"
-								:ride-id="item.rideId"
-								:expanded-routes="expandedRoutes"
-							/>
+						<template
+							#ride-item="{ item }: { item: { label: string, icon: string, orphans: Orphan[], rideId: string, buses: Bus[] } }">
+							<AdminDashboardRideItem :icon="item.icon" :label="item.label"
+								:orphans="item.orphans" :buses="item.buses"
+								:ride-id="item.rideId" :expanded-routes="expandedRoutes"
+								@orphan-assigned="refetchGetRides" />
 						</template>
 
 						<template #add-bus-item="{ item }: { item: { rideId: string } }">
-							<AdminDashboardAddBusSelector
-								v-if="buses"
-								:buses="buses"
-								:buses-loading="busesLoading"
-								:ride-id="item.rideId"
-								@bus-added="refetchGetRides"
-							/>
+							<AdminDashboardAddBusSelector v-if="buses" :buses="buses"
+								:buses-loading="busesLoading" :ride-id="item.rideId"
+								@bus-added="refetchGetRides" />
 						</template>
 					</UTree>
 				</div>
@@ -134,21 +110,21 @@ const expandedRoutes = ref<string[]>([]);
 </template>
 
 <style>
-.scroll-bar{
+.scroll-bar {
 	overflow-y: auto;
 	scrollbar-color: yellow;
 }
 
 .scroll-bar::-webkit-scrollbar {
-  width: 3px;
+	width: 3px;
 }
 
 .scroll-bar::-webkit-scrollbar-track {
-  border-radius: 11px;
+	border-radius: 11px;
 }
 
 .scroll-bar::-webkit-scrollbar-thumb {
-  background: #314158;
-  border-radius: 11px;
+	background: #314158;
+	border-radius: 11px;
 }
 </style>
--- components/admin-dashboard/ride-item/ride-item.vue
+++ components/admin-dashboard/ride-item/ride-item.vue
@@ -13,12 +13,17 @@ const props = defineProps<{
 	expandedRoutes: string[];
 }>();
 
+const emit = defineEmits<{
+	(e: "orphan-assigned"): void;
+}>();
+
 const tabItems = computed<TabsItem[]>(() => {
 	return props.buses.map(bus => ({
 		label: `Kola ${bus.busNumber}`,
 		value: bus.busNumber,
 	}));
 });
+const seatSelectionModalOpen = ref(false);
 
 const { selectedSeats } = useReservations(props.rideId);
 const selectSeat = (seats: number[], busNumber: number) => {
@@ -30,24 +35,30 @@ const selectSeat = (seats: number[], busNumber: number) => {
 	}];
 };
 
-const reserveSeat = (busNumber: number, orphan: Orphan) => {
+const reserveSeat = async (busNumber: number, orphan: Orphan) => {
 	const seatNumber = selectedSeats.value.find(seat => seat.busNumber === busNumber)?.seats[0];
-	const name = orphan.name;
-	const lastName = orphan.lastName;
-	const phone = orphan.phone;
 
 	const reservation = {
 		rideId: props.rideId,
-		name,
-		lastName,
-		phone,
-		reservations: {
-			busNumber,
-			seats: [seatNumber],
-		},
+		orphan: orphan,
+		busNumber: busNumber,
+		seat: seatNumber,
 	};
 
+	try {
+		await $fetch("/apis/admin/assignOrphan", {
+			method: "post",
+			body: reservation
+		});
+
+	}
+	catch (error) {
+		console.error(error);
+	}
 	console.log(reservation);
+	seatSelectionModalOpen.value = false
+	emit("orphan-assigned")
+
 };
 
 const isExpanded = computed(() => props.expandedRoutes.includes(props.rideId));
@@ -56,81 +67,54 @@ const isExpanded = computed(() => props.expandedRoutes.includes(props.rideId));
 <template>
 	<div class="flex justify-between items-center w-full">
 		<div class="flex gap-2 items-center px-2 py-1 rounded">
-			<Icon
-				:name="icon"
-				size="20"
-			/>
+			<Icon :name="icon" size="20" />
 
 			<p>{{ label }}</p>
 
-			<UModal
-				v-if="orphans.length > 0"
-				title="Neprijavljeni putnici"
-				description="Putnici koji su se prijavili na ovoj liniji, ali su izbrisani sa linije zbog promene autobusa."
-			>
-				<UButton
-					color="error"
-					class="cursor-pointer p-0 sm:p-0"
-					size="xl"
-					variant="link"
-					icon="material-symbols:warning-rounded"
-					@click="(e) => e.stopPropagation()"
-				/>
+			<UModal v-if="orphans.length > 0" title="Neprijavljeni putnici"
+				description="Putnici koji su se prijavili na ovoj liniji, ali su izbrisani sa linije zbog promene autobusa.">
+				<UButton color="error" class="cursor-pointer p-0 sm:p-0" size="xl" variant="link"
+					icon="material-symbols:warning-rounded" @click="(e) => e.stopPropagation()" />
 
 				<template #body>
 					<div class="flex flex-col gap-2">
-						<div
-							v-for="orphan in orphans"
-							:key="orphan.userId"
-							class="flex gap-2 items-center justify-between text-sm"
-						>
+						<div v-for="orphan in orphans" :key="orphan.userId"
+							class="flex gap-2 items-center justify-between text-sm">
 							<div class="flex gap-2 items-center">
-								<Icon
-									name="material-symbols:person-outline"
-									size="20"
-								/>
+								<Icon name="material-symbols:person-outline"
+									size="20" />
 
 								<p>{{ orphan.name }} {{ orphan.lastName }}</p>
 							</div>
 
-							<UModal
-								title="Autobus"
-								description="Izaberite novo sedište."
-							>
-								<UButton
-									label="Dodeli novo sedište"
-									color="secondary"
+							<UModal :open="seatSelectionModalOpen" title="Autobus"
+								description="Izaberite novo sedište.">
+								<UButton label="Dodeli novo sedište" color="secondary"
 									icon="material-symbols:add-circle-outline"
-									size="sm"
-									class="cursor-pointer"
-								/>
+									@click="seatSelectionModalOpen = true" size="sm"
+									class="cursor-pointer" />
 
 								<template #body>
 									<div class="flex flex-col gap-2">
-										<UTabs
-											:default-value="buses[0].busNumber"
-											:items="tabItems"
-											:ui="{
+										<UTabs :default-value="buses[0].busNumber"
+											:items="tabItems" :ui="{
 												list: tabItems.length > 1 ? '' : 'max-w-1/2',
-											}"
-										>
+											}">
 											<template #content="{ item }">
-												<div class="flex flex-col gap-5">
+												<div
+													class="flex flex-col gap-5">
 													<AppBusSeatLayout
 														:disabled="false"
 														:selected-seats="getSelectedSeatsForBus(selectedSeats, item.value as number)"
 														:rows="buses.find(bus => bus.busNumber === item.value)?.reservationSeatsRows ?? []"
 														:max-seats-reached="false"
-														@update:selected-seats="(seats) => selectSeat(seats, item.value as number)"
-													/>
+														@update:selected-seats="(seats) => selectSeat(seats, item.value as number)" />
 
-													<UButton
-														label="Potvrdi sedište"
+													<UButton label="Potvrdi sedište"
 														color="success"
 														icon="material-symbols:check-circle-outline"
 														class="cursor-pointer self-center"
-														@click="reserveSeat(item.value as number, orphan)"
-													/>
+														@click="reserveSeat(item.value as number, orphan)" />
 												</div>
 											</template>
 										</UTabs>
@@ -143,11 +127,8 @@ const isExpanded = computed(() => props.expandedRoutes.includes(props.rideId));
 			</UModal>
 		</div>
 
-		<Icon
-			name="lucide:chevron-down"
-			size="20"
+		<Icon name="lucide:chevron-down" size="20"
 			class="transform transition-transform duration-200 ease-in-out"
-			:class="{ 'rotate-180': isExpanded }"
-		/>
+			:class="{ 'rotate-180': isExpanded }" />
 	</div>
 </template>

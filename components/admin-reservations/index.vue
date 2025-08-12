a147de33e956392408ffad5b7c4b27b5f8a88b7f
Pavle <pavle.jovanovic@nqode.io>
2025-08-12 21:06:02 +0200

    feat: admin reservations (WIP)

---
+++ components/admin-reservations/index.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+import type { DateValue } from "@internationalized/date";
+import type { Location } from "../home/header/types";
+
+const { data: locations, error } = await useFetch<Location[]>("/apis/routes", {
+	onRequestError: (error) => {
+		console.error({
+			message: error.error.message,
+			response: error.response,
+			cause: error.error.cause,
+		});
+	},
+});
+
+const selectedDate = ref<DateValue>();
+const selectedLocation = ref<Location | undefined>();
+
+watchEffect(() => {
+	if (locations.value?.length) {
+		selectedLocation.value = locations.value[0];
+	}
+});
+</script>
+
+<template>
+	<div class="flex flex-col gap-5 ">
+		<h1 class="text-2xl font-bold text-center mb-5">
+			Admin Rezervacije
+		</h1>
+
+		<AdminReservationsHeader
+			v-model:selected-location="selectedLocation"
+			v-model:selected-date="selectedDate"
+			:locations="locations ?? []"
+			:error="!!error"
+		/>
+
+		<!-- TODO Create new component for admin reservations (this one is not compatible for reservation on behalf of other users) -->
+		<Transition>
+			<HomeReservations
+				v-if="selectedDate && selectedLocation"
+				:disable-seat-limit="true"
+				:selected-date="selectedDate"
+				:selected-location="selectedLocation"
+			/>
+		</Transition>
+	</div>
+</template>
+
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+  transition: all 0.5s ease-out;
+  overflow: hidden;
+}
+
+.v-enter-active {
+  transition-delay: 0.5s;
+}
+
+.v-leave-active {
+  transition-timing-function: ease-in;
+}
+
+.v-enter-from,
+.v-leave-to {
+  height: 0;
+  opacity: 0;
+  transform: translateY(-10px);
+}
+</style>
